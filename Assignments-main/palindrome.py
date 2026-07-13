66d1f99f8728e2ca765599a6efc19ff5984e2ddc
ankushsanghvi03 <ankush.sanghavi@prodapt.com>
2026-07-13 15:11:28 +0530

    Delete Assignments-main/palindrome.py

--- Assignments-main/palindrome.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(s):
-    s = s.lower()
-    return s == s[::-1]
-word = input("Enter a string: ")
-
-if is_palindrome(word):
-    print(f'"{word}" is a Palindrome.')
-else:
-    print(f'"{word}" not a Palindrome.')
