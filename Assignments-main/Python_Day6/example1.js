c5b3cea36e6842aefb5bca659b28f7fff7b87025
ankushsanghvi03 <ankush.sanghavi@prodapt.com>
2026-07-17 10:17:10 +0530

    Java Day1

---
+++ Assignments-main/Python_Day6/example1.js
@@ -0,0 +1,20 @@
+// let name = "Ankush"
+// const age = 30
+// var location = "Chennai"
+
+// name = "Ankita"
+// var location = "Bangalore"
+
+// console.log(name)
+// console.log(location)
+
+let name = "Ankush";
+const age = 30;
+var location = "Chennai";
+
+name = "Ankita";
+location = "Bangalore"; // No need to use var again
+
+console.log(name);
+console.log(location);
+console.log(age);
